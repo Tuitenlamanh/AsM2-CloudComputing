5cd32e75fae94447f8788c3b6aa1ed3ebfbf83d5
Nguyen Van Manh <98015760+Tuitenlamanh@users.noreply.github.com>
2022-03-26 11:29:16 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $severname = "localhost";
     $username = "root";
     $password = "";
-    $database = "cpasm2";
+    $database = "websdlc";
     //Khai báo biến để kết nỗi CSDL
     $connect = new mysqli($severname,$username,$password,$database);
     //$connect = new mysqli_connect ----> Hướng Thủ Tục
